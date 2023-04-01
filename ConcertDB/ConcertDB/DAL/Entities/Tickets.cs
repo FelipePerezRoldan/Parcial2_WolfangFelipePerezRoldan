6190c623cbaab0e60d0784a673eec549c4da6bc5
WOLFANG FELIPE P�REZ ROLD�N <wolfangperez302437@correo.itm.edu.co>
2023-04-01 18:21:02 -0500

    Creation of Properties Id, CreatedDate, and ModifiedDate in the Class Entity.cs Creation of Propierties UseDate, IsUsed, and EntranceGate in the Class Tickets.cs

--- ConcertDB/ConcertDB/DAL/Entities/Tickets.cs
+++ ConcertDB/ConcertDB/DAL/Entities/Tickets.cs
@@ -1,7 +1,16 @@
-﻿namespace ConcertDB.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcertDB.DAL.Entities
 {
     public class Tickets : Entity
     {
-
+        [Display(Name = "Ticket use date.")]
+        public DateTime UseDate { get; set; }
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [Display(Name = "Ticket use")]
+        public Boolean IsUsed { get; set; }
+        [Display(Name = "Entrace Gate")]
+        [MaxLength(10, ErrorMessage = "The field {0} must be shorter.")]
+        public String EntranceGate { get; set; }
     }
 }
